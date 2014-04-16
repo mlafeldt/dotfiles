86faa7b2984566f2ff60986202a664db1cf08fd7
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-04-16 22:26:13 +0200

    Install more Go tools

--- install.sh
+++ install.sh
@@ -40,6 +40,11 @@ install_go_tools() {
     # vim-gocode
     go get -v -u github.com/nsf/gocode
     go get -v -u code.google.com/p/go.tools/cmd/goimports
+    # misc
+    go get -v -u code.google.com/p/go.tools/cmd/cover
+    go get -v -u code.google.com/p/go.tools/present
+    go get -v -u github.com/mitchellh/gox
+    go get -v -u github.com/pengwynn/flint
 }
 
 install_tools() {
